f96e2825a232c579b0bf253919dc436bdadd1ed8
Olivia Hu <oliviahu2020@gmail.com>
2024-10-30 18:42:11 -0400

    Refactors to React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <div className="main">
+       <img className="profile" src={require('./images/profile_photo.jpeg')} alt={"Olivia Hu"}/>
+        
+        <div className="bio">
+          <div className="header">
+            <h1>Olivia Hu</h1>
+            <h2>Computer Science student at UPenn</h2>
+          </div>
+          <div className="body">
+            <p>
+              Hi! I'm <strong>Olivia Hu</strong>, a sophomore at the University of Pennsylvania studying computer science with minors in mathematics and economics. I'm currently developing my skills in software engineering and web/app development, and am eager to apply them to projects that make a difference! In my free time, I enjoy...
+            </p>
+            <ul>
+              <li>Reading</li>
+              <li>Going to concerts</li>
+              <li>Trying new restaurants</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="socials">
+        <a href="https://github.com/olivianhu">Github</a>
+        <a href="https://www.linkedin.com/in/olivianhu/">LinkedIn</a>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
